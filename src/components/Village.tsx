8e73bc6866dba4ea4e722009d6ba5b26addab2c7
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-04-03 17:36:06 -0300

    position buildings in city

--- src/components/Village.tsx
+++ src/components/Village.tsx
@@ -4,10 +4,9 @@ export function Village() {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
+    const canvas = canvasRef.current as any;
     const context = canvas.getContext("2d");
 
-    // Carregue a imagem da cidade
     const cityImage = new Image();
     cityImage.src = "/cityBackground.jpg";
     cityImage.onload = () => {
@@ -18,24 +17,22 @@ export function Village() {
       console.error("Erro ao carregar a imagem da cidade.");
     };
 
-    // Carregue as imagens das construções
     const buildingImages = [
       { src: "building1.png", x: 880, y: 150 },
-      { src: "building2.png", x: 100, y: 100 },
-      // { src: "building3.png", x: 100, y: 200 },
-      // { src: "building4.png", x: 100, y: 100 },
-      // { src: "building5.png", x: 100, y: 200 },
-      // { src: "building6.png", x: 100, y: 100 },
-      // { src: "building7.png", x: 100, y: 200 },
-      // { src: "building8.png", x: 100, y: 100 },
-      // { src: "building9.png", x: 100, y: 200 },
-      // { src: "building10.png", x: 100, y: 100 },
-      // { src: "building11.png", x: 100, y: 100 },
-      // { src: "building12.png", x: 100, y: 100 },
-      // { src: "building13.png", x: 100, y: 100 },
-      // { src: "building14.png", x: 100, y: 100 },
-      // { src: "building15.png", x: 100, y: 100 },
-      // Adicione mais construções conforme necessário
+      { src: "building2.png", x: 1220, y: 170 },
+      { src: "building3.png", x: 1520, y: 150 },
+      { src: "building4.png", x: 1600, y: 490 },
+      { src: "building5.png", x: 500, y: 500 },
+      { src: "building6.png", x: 1150, y: 450 },
+      { src: "building7.png", x: 1030, y: 760 },
+      { src: "building8.png", x: 1340, y: 560 },
+      { src: "building9.png", x: 350, y: 380 },
+      { src: "building10.png", x: 1200, y: 700 },
+      { src: "building11.png", x: 870, y: 450 },
+      { src: "building12.png", x: 1300, y: 300 },
+      { src: "building13.png", x: 500, y: 200 },
+      { src: "building14.png", x: 830, y: 650 },
+      { src: "building15.png", x: 840, y: 330 },
     ];
 
     buildingImages.forEach((building) => {
