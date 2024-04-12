b139e7beb91222a9e6cbd5cf668649e78a335c1b
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-04-12 14:59:47 -0300

    fix: mover o mouse fora da building

--- src/components/Village.tsx
+++ src/components/Village.tsx
@@ -22,10 +22,9 @@ export function Village() {
   const [selectedBuilding, setSelectedBuilding] = useState<any>(null);
 
   function handleBuildingClick(building: any) {
-    if (building && selectedBuilding && building.id === selectedBuilding.id) {
+    if (building) {
+      setSelectedBuilding(building);
       setIsModalOpen(true);
-    } else {
-      setIsModalOpen(false);
     }
   }
 
@@ -55,19 +54,6 @@ export function Village() {
     const buildingImages = [
       { id: 1, src: "buildings/building1.png", x: 880, y: 150 },
       { id: 2, src: "buildings/building2.png", x: 1220, y: 170 },
-      { id: 3, src: "buildings/building3.png", x: 1520, y: 150 },
-      { id: 4, src: "buildings/building4.png", x: 1600, y: 490 },
-      { id: 5, src: "buildings/building5.png", x: 500, y: 500 },
-      { id: 6, src: "buildings/building6.png", x: 1150, y: 450 },
-      { id: 7, src: "buildings/building7.png", x: 1030, y: 760 },
-      { id: 8, src: "buildings/building8.png", x: 1340, y: 560 },
-      { id: 9, src: "buildings/building9.png", x: 350, y: 380 },
-      { id: 10, src: "buildings/building10.png", x: 1200, y: 700 },
-      { id: 11, src: "buildings/building11.png", x: 870, y: 450 },
-      { id: 12, src: "buildings/building12.png", x: 1300, y: 300 },
-      { id: 13, src: "buildings/building13.png", x: 500, y: 200 },
-      { id: 14, src: "buildings/building14.png", x: 830, y: 650 },
-      { id: 15, src: "buildings/building15.png", x: 840, y: 330 },
     ];
 
     const buildings = buildingImages.map((building) => {
@@ -75,50 +61,6 @@ export function Village() {
       img.src = building.src;
       return { ...building, img };
     });
-
-    const aircraftImage = new Image();
-    aircraftImage.src = "/effects/farmBlade.png";
-
-    const smokeImage = new Image();
-    smokeImage.src = "/effects/smoke.png";
-    let smokes = [] as any;
-
-    const rippleImage = new Image();
-    rippleImage.src = "/effects/ripple.png";
-
-    // Array para armazenar os pontos de animação de ripple
-    const ripplePoints = [
-      { x: 770, y: 190 },
-      { x: 770, y: 200 },
-      { x: 770, y: 210 },
-      { x: 770, y: 220 },
-      { x: 770, y: 230 },
-    ];
-
-    // Array para armazenar o deslocamento vertical das ondulações
-    const rippleOffsets = new Array(ripplePoints.length).fill(0);
-
-    // Adiciona a animação de ripple nos pontos demarcados
-    function drawRippleEffects() {
-      ripplePoints.forEach((point, index) => {
-        const offsetY = rippleOffsets[index];
-        // Calcula a opacidade com base na posição vertical da ondulação
-        const opacity =
-          1 -
-          Math.abs(offsetY - rippleImage.height / 2) / (rippleImage.height / 2);
-        context.globalAlpha = opacity;
-        context.drawImage(rippleImage, point.x, point.y + offsetY);
-        context.globalAlpha = 1; // Restaura a opacidade para o valor padrão
-      });
-    }
-
-    // Atualiza a posição das ondulações para criar o efeito de animação
-    function animateRipples() {
-      rippleOffsets.forEach((offset, index) => {
-        // Reduzir a velocidade de deslocamento vertical
-        rippleOffsets[index] = (offset + 0.1) % rippleImage.height;
-      });
-    }
 
     canvas.addEventListener("mousemove", handleMouseMove);
 
@@ -154,6 +96,34 @@ export function Village() {
       return null;
     }
 
+    let dragStart = null as any;
+
+    canvas.addEventListener("mousedown", (event: any) => {
+      dragStart = { x: event.clientX, y: event.clientY };
+    });
+
+    canvas.addEventListener("mouseup", (event: any) => {
+      if (dragStart) {
+        const dragEnd = { x: event.clientX, y: event.clientY };
+        const dragDistance = Math.hypot(
+          dragEnd.x - dragStart.x,
+          dragEnd.y - dragStart.y
+        );
+
+        // Consider it a click if the mouse has moved less than 10 pixels.
+        if (dragDistance < 10) {
+          const building = getMouseOverBuilding();
+          if (building && building.id === currentBuildingId) {
+            handleBuildingClick(building);
+          } else {
+            setSelectedBuilding(null);
+            setIsModalOpen(false);
+          }
+        }
+      }
+      dragStart = null; // Reset dragStart for the next drag operation.
+    });
+
     function animate() {
       context.clearRect(0, 0, canvas.width, canvas.height);
       context.save();
@@ -170,11 +140,12 @@ export function Village() {
         context.drawImage(cityImage, 0, 0, 2100, 1128);
       }
 
+      const mouseOverBuilding = getMouseOverBuilding();
+
       buildings.forEach((building) => {
         if (building.img.complete) {
           context.drawImage(building.img, building.x, building.y);
 
-          const mouseOverBuilding = getMouseOverBuilding();
           if (mouseOverBuilding && mouseOverBuilding === building) {
             context.strokeStyle = "red";
             context.strokeRect(
@@ -191,41 +162,6 @@ export function Village() {
         }
       });
 
-      if (aircraftImage.complete) {
-        context.save();
-        context.translate(1745, 540);
-        context.rotate(rotation);
-        context.drawImage(
-          aircraftImage,
-          -aircraftImage.width / 2,
-          -aircraftImage.height / 2
-        );
-        context.restore();
-      }
-
-      smokes.forEach((smoke: any, index: number) => {
-        context.globalAlpha = smoke.opacity;
-        context.drawImage(smokeImage, smoke.x, smoke.y);
-        context.globalAlpha = 1;
-        smoke.y -= 0.5; // Reduz a velocidade vertical
-        smoke.x -= 0.2; // Movimento horizontal para a esquerda
-        smoke.opacity -= 0.005; // Reduz a opacidade de forma mais lenta
-        if (smoke.opacity <= 0) {
-          smokes.splice(index, 1);
-        }
-      });
-
-      if (Math.random() < 0.02) {
-        smokes.push({
-          x: 400,
-          y: 340,
-          opacity: 1,
-        });
-      }
-
-      drawRippleEffects();
-      animateRipples();
-
       rotation += 0.01;
 
       context.restore();
@@ -233,15 +169,6 @@ export function Village() {
     }
 
     animate();
-
-    canvas.addEventListener("click", () => {
-      const building = getMouseOverBuilding();
-      if (building) {
-        console.log(`Clique detectado no edifício com ID: ${building.id}`);
-        setSelectedBuilding(building);
-        handleBuildingClick(building);
-      }
-    });
 
     return () => {
       canvas.removeEventListener("mousemove", handleMouseMove);
@@ -262,15 +189,7 @@ export function Village() {
           <canvas ref={canvasRef} />
         </TransformComponent>
       </TransformWrapper>
-      <Dialog
-        open={isModalOpen}
-        onOpenChange={(isOpen) => {
-          setIsModalOpen(isOpen);
-          if (!isOpen) {
-            setSelectedBuilding(null); // Limpar selectedBuilding ao fechar o modal
-          }
-        }}
-      >
+      <Dialog open={isModalOpen} onOpenChange={() => setIsModalOpen(false)}>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
             <DialogTitle>Building Details</DialogTitle>
@@ -285,7 +204,7 @@ export function Village() {
               </Label>
               <Input
                 id="name"
-                value={selectedBuilding ? selectedBuilding.id : ""} // Definir como string vazia se selectedBuilding for null
+                value={selectedBuilding ? selectedBuilding.id : ""}
                 className="col-span-3"
                 readOnly
               />
@@ -296,7 +215,11 @@ export function Village() {
               </Label>
               <Input
                 id="username"
-                value={`x: ${selectedBuilding?.x}, y: ${selectedBuilding?.y}`}
+                value={
+                  selectedBuilding
+                    ? `x: ${selectedBuilding.x}, y: ${selectedBuilding.y}`
+                    : ""
+                }
                 className="col-span-3"
                 readOnly
               />
